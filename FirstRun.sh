34630a74bd8062a13c12248be4a92fb11ca12003
Francisco Suarez <franksg88@gmail.com>
2015-11-03 17:48:14 -0600

    Agregada funcion de inicio (crea enlances simbolicos)

---
+++ FirstRun.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# This script needed for create the symlinks for th resources.
+
+echo "Create the whole container"
+mkdir -p  $HOME/.odf
+
+dots=$HOME/.odf
+configs=$HOME/.config/
+
+cp -R * $dots
+
+delete_all() {
+	if  [[ -d "${1}" ]]; then
+		rm -R ${1}
+	fi
+}
+
+# ln options
+symlink="ln -s -f -n"
+
+#COMPTON
+echo "Comptonize your Linux"
+delete_all $configs/compton
+$symlink $dots/compton $configs
+
+#dunst
+echo "Dunst...something your Linux"
+delete_all $configs/dunts
+$symlink $dots/dunst $configs
+
+#OPENBOX
+echo "Openboxing your Linux"
+delete_all $configs/openbox
+$symlink $dots/openbox $configs
+
+#RANGER
+echo "A ranger inyour Linux? what's about a rogue?"
+delete_all $configs/ranger
+$symlink $dots/ranger $configs
+
+#MPD
+echo "A daemon Music in your Linux"
+delete_all $HOME/.mpd
+$symlink $dots/.mpd $HOME
+
+#NCMPCPP
+echo "Lets control that Daemon in your Linux!!!"
+delete_all $HOME/.ncmpcpp
+$symlink $dots/.ncmpcpp $HOME
+
+#VIM
+echo "Some configs for your Linux!!!"
+delete_all $HOME/.vimrc
+$symlink $dots/vimrc $HOME/.vimrc
+#XRESOURCES
+delete_all $HOME/.Xresources
+$symlink $dots/Xresources $HOME/.Xresources
+echo "xrdb $HOME/.Xresources" >> $configs/openbox/autostart
+#mpdscribble
+delete_all $HOME/.mpdscribble/
+$symlink $dots/mpdscribble $HOME/.mpdscribble
+#bashrc
+delete_all $HOME/.bashrc
+$symlink $dots/bashrc $HOME/.bashrc
+
+echo "DONE !!!" && notify-send "DONE !!!"
