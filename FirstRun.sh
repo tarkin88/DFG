0f137a8c6f44506d8b98eb5934c8c8722b7eecbf
Francisco Suarez <franksg88@gmail.com>
2015-12-09 18:34:58 -0600

    Actualizando y mejorando openbox

--- FirstRun.sh
+++ FirstRun.sh
@@ -2,9 +2,9 @@
 # This script needed for create the symlinks for th resources.
 
 echo "Create the whole container"
-mkdir -p  $HOME/.odf
+mkdir -p  $HOME/.dotfiles
 
-dots=$HOME/.odf
+dots=$HOME/.dotfiles
 configs=$HOME/.config/
 
 cp -R * $dots
@@ -24,27 +24,27 @@ delete_all $configs/compton
 $symlink $dots/compton $configs
 
 #dunst
-#echo "Dunst...something your Linux"
-#delete_all $configs/dunts
-#$symlink $dots/dunst $configs
+echo "Dunst...something your Linux"
+delete_all $configs/dunts
+$symlink $dots/dunst $configs
 
 #tint2
-#echo "Tinting your Linux"
-#delete_all $configs/tint2
-#$symlink $dots/tint2 $configs
+echo "Tinting your Linux"
+delete_all $configs/tint2
+$symlink $dots/tint2 $configs
 
 #OPENBOX
-#echo "Openboxing your Linux"
-#delete_all $configs/openbox
-#$symlink $dots/openbox $configs
+echo "Openboxing your Linux"
+delete_all $configs/openbox
+$symlink $dots/openbox $configs
 
 #RANGER
-echo "A ranger inyour Linux? what's about a rogue?"
+echo "A ranger in your Linux? what's about a rogue?"
 delete_all $configs/ranger
 $symlink $dots/ranger $configs
 
 #VIM
-echo "Some configs for your Linux!!!"
+echo "Some extra configs for your Linux!!!"
 delete_all $HOME/.vimrc
 $symlink $dots/vimrc $HOME/.vimrc
 #XRESOURCES
@@ -54,17 +54,17 @@ $symlink $dots/Xresources $HOME/.Xresources
 delete_all $HOME/.bashrc
 $symlink $dots/bashrc $HOME/.bashrc
 #mpd
-#delete_all $HOME/.mpd
-#$symlink $dots/mpd $HOME/.mpd
-
-#delete_all $HOME/.ncmpcpp
-#$symlink $dots/ncmpcpp $HOME/.ncmpcpp
-
-#delete_all $HOME/.term.png
-#$symlink $dots/term.png $HOME/.term.png
-
+delete_all $HOME/.mpd
+$symlink $dots/mpd $HOME/.mpd
+#ncmpcpp
+delete_all $HOME/.ncmpcpp
+$symlink $dots/ncmpcpp $HOME/.ncmpcpp
+#conky
 delete_all $HOME/.conkyrc
 $symlink $dots/conkyrc $HOME/.conkyrc
+#openbox_theme
+delete_all $HOME/.themes/Dyne
+$symlink $dots/themes $HOME/.themes
 
 xrdb $HOME/.Xresources
 
