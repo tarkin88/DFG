51460927854b2af2efa6166472917d2d8a92305b
Francisco Suarez <franksg88@gmail.com>
2016-10-13 08:17:26 -0500

    limpieza general

--- FirstRun.sh
+++ FirstRun.sh
@@ -18,14 +18,6 @@ delete_all() {
 # ln options
 symlink="ln -s -f -n"
 
-#i3
-#echo "Let's try i3...again xD"
-#delete_all $configs/i3
-#$symlink $dots/i3 $configs
-
-#i3 blocks
-#delete_all $configs/i3blocks
-#$symlink $dots/i3blocks $configs
 #SCRIPTS
 delete_all $configs/bash_scripts
 $symlink $dots/bash_scripts $configs
@@ -40,16 +32,6 @@ echo "Dunst...something your Linux"
 delete_all $configs/dunts
 $symlink $dots/dunst $configs
 
-#termite
-#echo "Some colors for terminal"
-#delete_all $configs/termite
-$#symlink $dots/termite $configs
-
-#tint2
-echo "Tinting your Linux"
-delete_all $configs/tint2
-$symlink $dots/tint2 $configs
-
 # i3
 echo "i3 to me!"
 delete_all $configs/i3
@@ -57,12 +39,6 @@ $symlink $dots/i3 $configs
 
 delete_all $configs/i3blocks
 $symlink $dots/i3blocks $configs
-
-#OPENBOX
-#echo "Openboxing your Linux"
-#delete_all $configs/openbox
-#$symlink $dots/openbox $configs
-#obmenu-generator -s -p -R
 
 #RANGER
 echo "A ranger in your Linux? what's about a rogue?"
@@ -73,9 +49,6 @@ $symlink $dots/ranger $configs
 echo "Some extra configs for your Linux!!!"
 delete_all $HOME/.vimrc
 $symlink $dots/vimrc $HOME/.vimrc
-#NVIM
-#delete_all $configs/nvim
-#$symlink $dots/nvim $configs
 
 #XRESOURCES
 delete_all $HOME/.Xresources
@@ -85,30 +58,16 @@ $symlink $dots/Xresources $HOME/.Xresources
 delete_all $HOME/.xinitrc
 $symlink $dots/xinitrc $HOME/.xinitrc
 
-#bashrc
-delete_all $HOME/.bashrc
-$symlink $dots/bashrc $HOME/.bashrc
 #mpd
-delete_all $HOME/.mpd
-$symlink $dots/mpd $HOME/.mpd
-#ncmpcpp
-delete_all $HOME/.ncmpcpp
-$symlink $dots/ncmpcpp $HOME/.ncmpcpp
+delete_all $configs/mpd
+$symlink $dots/mpd $configs
 
-#Wallpapers
-delete_all $HOME/.walls
-$symlink $dots/walls  $HOME/.walls
-#conky
-#delete_all $HOME/.conkyrc
-#$symlink $dots/conkyrc $HOME/.conkyrc
-##openbox_
-#delete_all $HOME/.themes
-#$symlink $dots/themes  $HOME/.themes
 echo "maybe some theme too..."
 xrdb $HOME/.Xresources
 echo "MPD:"
 mpc update | head -1
 chmod +x $configs/bash_scripts/*
+chmod +x $configs/i3blocks/*
 echo ""
 echo "DONE"
 dunstify  "That's all folks!!!"
