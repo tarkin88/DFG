039d597e20855ac01e2ad346a468f52a06ee56e3
Francisco Suarez <frank@localhost.localdomain>
2017-02-21 21:22:45 +0000

    Actulizada configuración para el uso de laptop DELL

--- bash_scripts/autostart.sh
+++ bash_scripts/autostart.sh
@@ -10,9 +10,10 @@ setxkbmap latam &
 xrdb ~/.Xresources
 nitrogen --restore &
 (sleep 1s && pulseaudio --start) &
-(sleep 2s && compton -cCzG -t-3 -l-5 -r4 --config /dev/null --backend xrender  --unredir-if-possible) &
+#(sleep 2s && compton -cCzG -t-3 -l-5 -r4 --config /dev/null --backend xrender  --unredir-if-possible) &
 (sleep 2s && dunst) &
 (sleep 2s && devmon --sync --exec-on-drive "notify-send 'Automount' 'New media detect: $media.'" --exec-on-remove "notify-send 'Automount' '$media has removed.'") &
 #(sleep 3s && mpd ~/.mpd/config) &
 (sleep 3s && clipit) &
+(sleep 3s && compton --config ~/.config/compton/compton.conf) &
 (sleep 120s & ~/.config/bash_scripts/check_updates.sh ) &
