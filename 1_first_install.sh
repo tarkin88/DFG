1075966284ba4663e05e1bfe5c2206c46548e077
Francisco <franksg88@gmail.com>
2017-12-21 20:28:11 -0600

    added shiji font for polybar

--- 1_first_install.sh
+++ 1_first_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-yaourt -S arc-gtk-theme ccache clipit termite termite-terminfo compton dmenu feh  galculator gnome-mplayer gnome-ssh-askpass2 gparted gtk-engine-murrine highlight atool htop i3-gaps-git i3lock-color-git i3status imagemagick lm_sensors lxappearance mpc mpd mplayer ncmpcpp net-tools ntfs-3g numlockx oblogout-fork-git openssh paper-icon-theme pcmanfm polkit-gnome polybar poppler ranger rofi-git rxvt-unicode rxvt-unicode-terminfo scrot slim teamviewer transmission-gtk ttf-font-awesome ttf-monaco udevil udiskie udisks2 urxvt-clipboard viewnior xorg-xbacklight xorg-xinit xorg-xkill xorg-xrandr xss-lock --noconfirm
+yaourt -S arc-gtk-theme ccache clipit termite termite-terminfo compton dmenu feh  galculator gnome-mplayer gnome-ssh-askpass2 gparted gtk-engine-murrine highlight atool htop i3-gaps-git i3lock-color-git i3status imagemagick lm_sensors lxappearance mpc mpd mplayer ncmpcpp net-tools ntfs-3g numlockx oblogout-fork-git openssh paper-icon-theme pcmanfm polkit-gnome polybar poppler ranger rofi-git rxvt-unicode rxvt-unicode-terminfo scrot slim teamviewer transmission-gtk ttf-font-awesome ttf-monaco udevil udiskie udisks2 urxvt-clipboard viewnior xorg-xbacklight xorg-xinit xorg-xkill xorg-xrandr xss-lock siji-git --noconfirm
 
 echo "Done, now execute generate.sh"
