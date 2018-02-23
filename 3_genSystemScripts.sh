f649d8bacec3d3e778996d831a41af1153134c40
Francisco Suárez <franksg88@gmail.com>
2018-02-22 19:23:56 -0600

    clean

--- 3_genSystemScripts.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/sh
-
-echo "Create a symlink for lock and screenshot  scripts"
-echo " . "
-sudo ln -s ~/.config/bash_scripts/lock.sh /usr/local/bin/lock
-echo " . . "
-sudo ln -s ~/.config/bash_scripts/screenshot.sh /usr/local/bin/screenshoot
-echo "Done"
