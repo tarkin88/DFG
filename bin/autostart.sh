324435257af9ae8db7e88e4ab21b44d79799f15a
Francisco Suarez <franksg88@gmail.com>
2020-03-15 22:20:06 -0600

    netter compositor config

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -13,7 +13,7 @@ sleep 1s; ~/.bin/launch_polybar.sh &
 sleep 2s; dunst &
 #sleep 2s; ~/.bin/bt-connect.sh &
 sleep 3s; picom -b --experimental-backends --dbus --config ~/.config/picom.conf &
-sleep 5s; redshift -l "20.671955:-103.416504" -t 6700:3500 -g 0.8 -m vidmode &
+sleep 5s; redshift -l "20.671955:-103.416504" -t 7700:5000 -g 0.8 -m vidmode &
 #sleep 4s; [ ! -s ~/.config/mpd/pid ] && mpd &
 sleep 5s; /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 sleep 6s; udiskie -aT &
